e175705272d4f727cf84191116c755853c8f9f17
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-10 19:05:50 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,10 +19,18 @@ local function round1()
         for j = 1, 5 do
             for k = 1, wSize do
                 state[i][j][k] = xor(state[i][j][k], state[i][(j - 2) % 4 + 1][k])
-                state[i][j][k] = xor(state[i][j][k], state[i][j % 4 + 1][(k - 2) % 4 + 1])
+                state[i][j][k] = xor(state[i][j][k], state[i][j % 4 + 1][(k - 2) % wSize + 1])
             end
         end
     end
 end
 
-local function round2()
+local function round3()
+    local newState = {}
+    for i = 1, 5 do
+        newState[i] = {}
+        for j = 1, 5 do
+            newState[j][(2 * i + 3 * j - 1) % 4 + 1] = state[i][j]
+        end
+    end
+end
