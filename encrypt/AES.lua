a73f4de2ce5682eb0e5783b683326ad30a105865
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-21 16:11:03 -0400

    Update AES.lua

--- encrypt/AES.lua
+++ encrypt/AES.lua
@@ -1,1 +1,13 @@
+local function expandKey(k)
+    loca
 
+local function unsafeEncrypt(msg, key) --Every parameter is assumed to be safe
+    local state = {}
+    for i = 1, 4 do
+        state[i] = {}
+        for j = 1, 4 do
+            local index = i + j * 4
+            state[i][j] = msg:sub(index * 2 - 1, index * 2)
+        end
+    end
+end
