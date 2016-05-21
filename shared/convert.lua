bb8a13c53567c19b89fc82cc3b5dd023975b3cef
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-21 17:22:11 -0400

    Update convert.lua

--- shared/convert.lua
+++ shared/convert.lua
@@ -15,5 +15,48 @@ function btoh(b)
     return ("%x"):format(tonumber(b, 2))
 end
 
+function htob(h)
+    return ntob(hton(h))
+end
+
 function ntob(n)
-    return 
+    local size = math.floor(math.log(n, 2))
+    local bits = ""
+    for i = size, 0 do
+        local power = 2 ^ i
+        if n >= power then
+            bits = "1" .. bits
+            n = n - power
+        else
+            bits = "0" .. bits
+        end
+    end
+    return bits
+end
+
+function not(h)
+    local b = htob(h)
+    b = b:gsub("1", "2") -- 'cause I can
+    b = b:gsub("0", "1")
+    b = b:gsub("2", "0")
+    return btoh(b)
+end
+
+local function hOperation(funct)
+    return function(h1, h2)
+        b1, b2 = htob(h1), htob(h2)
+        local b3
+        for i = 1, math.min(#b1, #b2) do
+            local bool1, bool2 = b1:sub(i, i) == "1", b2:sub(i, i) == "1"
+            b3 = b3 .. ((funct(bool1, bool2) and "1") or "0")
+        end
+        if #b1 > #b2 then
+            b3 = 
+        h3 = btoh(b3)
+        return b3
+    end
+end
+
+and = hOperation(function(bool1, bool2) return bool1 and bool2 end)
+or = hOperation(function(bool1, bool2) return bool1 or bool2 end)
+xor = hOperation(function(bool1, bool2) return (bool2 and (not bool1)) or bool1 end) -- I'm 1, and I'm flipping you (1) off :)
