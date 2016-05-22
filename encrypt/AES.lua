6bc58c4a1a015c91164a9eea2062699e7a92a71c
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-22 11:40:08 -0400

    Update AES.lua

--- encrypt/AES.lua
+++ encrypt/AES.lua
@@ -7,19 +7,21 @@ local function expandKey(k)
 
 function encrypt(msg, key) --Every parameter is assumed to be safe
     if (#msg ~= 32) or ((#key ~= 32) and (#key ~= 48) and (#key ~= 64)) then error("Incorrect message or key size", 2) end
-    local state = {}
+    local state, tKey = {}, {}
     local nb, nk = #msg / 8, #key / 8 --because we divide by rows (4) and then by the number of charicters in a byte (2) to get the bytes in a row
+    local nr = math.max(nb, nk) + 6
     for j = 1, nb do
         for i = 1, 4 do
-            if i == j then state[i] = {} end
+            local index = i + j * 4
+            if j == 1 then state[i] = {} end
             state[i][j] = msg:sub(index * 2 - 1, index * 2)
         end
     end
-    local key = {}
-    for i = 1, 4 do
-        key[i] = {}
-        for j = 1, nk do
-            key[i][j] = key:sub(index * 2 - 1, index * 2)
+    for j = 1, nk do
+        for i = 1, 4 do
+            local index = i + j * 4
+            if j == 1 then tKey[i] = {}
+            tKey[i][j] = key:sub(index * 2 - 1, index * 2)
         end
     end
 end
