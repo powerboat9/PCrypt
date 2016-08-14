c66695e8d56c059406c243fbf25d3feaf60fbd6b
powerboat9 <powerboat9@users.noreply.github.com>
2016-08-14 01:19:50 -0400

    Update sha512.lua

--- hash/sha512.lua
+++ hash/sha512.lua
@@ -2,9 +2,8 @@ function hash512(msg)
     assert(type(msg) == "string", "Invalid input")
     local bin = convert.stob(msg) .. "1"
     local len = #bin
-    local add = (len % 1024)
-    if add < 0 then
-        add = 
+    local add = ((len % 1024) - 896) % 1024
+    bin = bin 
 
 local function genNewH(h)
     assert(convert.isHex(h, 8), "Invalid input")
