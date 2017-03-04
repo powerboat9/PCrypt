49ef4b54f440506852360b200119d3efd08c3f5d
powerboat9 <powerboat9@users.noreply.github.com>
2017-03-04 15:48:10 -0500

    Update AES.lua

--- AES.lua
+++ AES.lua
@@ -1,3 +1,28 @@
+local function split(str, size)
+    local t = {}
+    local i, j = 1, #str
+    for i, math.ceil(#str / size)
+        t[i] = str:sub((i - 1) * size + 1, i * size)
+    end
+    return t
+end
+
+local function pad(str, i)
+    return str .. ("\00"):rep(i - #str)
+end
+
+local function gcf(i, j)
+    if j > i then
+        i, j = j, i
+    end
+    local n
+    while true do
+        n = i % j
+        if n == 0
+
+local function lcm(i, j)
+    local div = gcf(i, j)
+
 function encrypt(data, key)
     data = data or "\00":rep(128)
     if type(data) ~= "string" then
